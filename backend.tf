0e5464de7ae34317a65584b78a95c23b11cff07c
Deepak Tiwari <deepurajpandit2@gmail.com>
2024-03-04 15:11:18 +0530

    jenkinsServerUsingTerraform

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mybucketlock1"
-    key    = "jenkins/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-  description = "VPC CIDR"
-  type        = string
-}
-
-variable "public_subnets" {
-  description = "Subnets CIDR"
-  type        = list(string)
-}
